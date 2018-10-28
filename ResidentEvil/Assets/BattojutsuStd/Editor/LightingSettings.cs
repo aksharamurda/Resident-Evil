c8afeec8a973e24b72939d30626fe1dfa6570f7b
aksharamurda <43597885+aksharamurda@users.noreply.github.com>
2018-10-28 19:17:29 +0700

    Update ResidentEvil/Assets/BattojutsuStd/Editor/LightingSettings.cs

--- ResidentEvil/Assets/BattojutsuStd/Editor/LightingSettings.cs
+++ ResidentEvil/Assets/BattojutsuStd/Editor/LightingSettings.cs
@@ -3,100 +3,102 @@ using UnityEngine;
 using UnityEditor;
 using System.Reflection;
 
-static class LightingSettings {
-
-
-    static SerializedObject s_SourceLightmapSettings;
-    static SerializedObject s_SourceRenderSettings;
+namespace BattojutsuStd.Editor
+{
+    static class LightingSettings
+    {
+        static SerializedObject s_SourceLightmapSettings;
+        static SerializedObject s_SourceRenderSettings;
 
 #if UNITY_2018_2_OR_NEWER
-    const string k_CopySettingsMenuPath = "Window/Rendering/Copy Lighting Settings";
-    const string k_PasteSettingsMenuPath = "Window/Rendering/Paste Lighting Settings";
+        const string k_CopySettingsMenuPath = "Window/Rendering/Copy Lighting Settings";
+        const string k_PasteSettingsMenuPath = "Window/Rendering/Paste Lighting Settings";
 #else
     const string k_CopySettingsMenuPath = "Window/Lighting/Copy Settings";
     const string k_PasteSettingsMenuPath = "Window/Lighting/Paste Settings";
 #endif
 
-    [MenuItem(k_CopySettingsMenuPath, priority = 200)]
-    static void CopySettings()
-    {
-        UnityEngine.Object lightmapSettings;
-        if (!TryGetSettings(typeof(LightmapEditorSettings), "GetLightmapSettings", out lightmapSettings))
-            return;
-
-        UnityEngine.Object renderSettings;
-        if (!TryGetSettings(typeof(RenderSettings), "GetRenderSettings", out renderSettings))
-            return;
-
-        s_SourceLightmapSettings = new SerializedObject(lightmapSettings);
-        s_SourceRenderSettings = new SerializedObject(renderSettings);
-    }
-
-    [MenuItem(k_PasteSettingsMenuPath, priority = 201)]
-    static void PasteSettings()
-    {
-        UnityEngine.Object lightmapSettings;
-        if (!TryGetSettings(typeof(LightmapEditorSettings), "GetLightmapSettings", out lightmapSettings))
-            return;
-
-        UnityEngine.Object renderSettings;
-        if (!TryGetSettings(typeof(RenderSettings), "GetRenderSettings", out renderSettings))
-            return;
-
-        CopyInternal(s_SourceLightmapSettings, new SerializedObject(lightmapSettings));
-        CopyInternal(s_SourceRenderSettings, new SerializedObject(renderSettings));
-
-        UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
-    }
-
-    [MenuItem(k_PasteSettingsMenuPath, validate = true)]
-    static bool PasteValidate()
-    {
-        return s_SourceLightmapSettings != null && s_SourceRenderSettings != null;
-    }
-
-    static void CopyInternal(SerializedObject source, SerializedObject dest)
-    {
-        var prop = source.GetIterator();
-        while (prop.Next(true))
+        [MenuItem(k_CopySettingsMenuPath, priority = 200)]
+        static void CopySettings()
         {
-            var copyProperty = true;
-            foreach (var propertyName in new[] { "m_Sun", "m_FileID", "m_PathID", "m_ObjectHideFlags" })
+            UnityEngine.Object lightmapSettings;
+            if (!TryGetSettings(typeof(LightmapEditorSettings), "GetLightmapSettings", out lightmapSettings))
+                return;
+
+            UnityEngine.Object renderSettings;
+            if (!TryGetSettings(typeof(RenderSettings), "GetRenderSettings", out renderSettings))
+                return;
+
+            s_SourceLightmapSettings = new SerializedObject(lightmapSettings);
+            s_SourceRenderSettings = new SerializedObject(renderSettings);
+        }
+
+        [MenuItem(k_PasteSettingsMenuPath, priority = 201)]
+        static void PasteSettings()
+        {
+            UnityEngine.Object lightmapSettings;
+            if (!TryGetSettings(typeof(LightmapEditorSettings), "GetLightmapSettings", out lightmapSettings))
+                return;
+
+            UnityEngine.Object renderSettings;
+            if (!TryGetSettings(typeof(RenderSettings), "GetRenderSettings", out renderSettings))
+                return;
+
+            CopyInternal(s_SourceLightmapSettings, new SerializedObject(lightmapSettings));
+            CopyInternal(s_SourceRenderSettings, new SerializedObject(renderSettings));
+
+            UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
+        }
+
+        [MenuItem(k_PasteSettingsMenuPath, validate = true)]
+        static bool PasteValidate()
+        {
+            return s_SourceLightmapSettings != null && s_SourceRenderSettings != null;
+        }
+
+        static void CopyInternal(SerializedObject source, SerializedObject dest)
+        {
+            var prop = source.GetIterator();
+            while (prop.Next(true))
             {
-                if (string.Equals(prop.name, propertyName, System.StringComparison.Ordinal))
+                var copyProperty = true;
+                foreach (var propertyName in new[] { "m_Sun", "m_FileID", "m_PathID", "m_ObjectHideFlags" })
                 {
-                    copyProperty = false;
-                    break;
+                    if (string.Equals(prop.name, propertyName, System.StringComparison.Ordinal))
+                    {
+                        copyProperty = false;
+                        break;
+                    }
                 }
+
+                if (copyProperty)
+                    dest.CopyFromSerializedProperty(prop);
             }
 
-            if (copyProperty)
-                dest.CopyFromSerializedProperty(prop);
+            dest.ApplyModifiedProperties();
         }
 
-        dest.ApplyModifiedProperties();
-    }
-
-    static bool TryGetSettings(System.Type type, string methodName, out UnityEngine.Object settings)
-    {
-        settings = null;
-
-        var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic);
-        if (method == null)
+        static bool TryGetSettings(System.Type type, string methodName, out UnityEngine.Object settings)
         {
-            Debug.LogErrorFormat("CopyLightingSettings: Could not find {0}.{1}", type.Name, methodName);
-            return false;
-        }
+            settings = null;
 
-        var value = method.Invoke(null, null) as UnityEngine.Object;
-        if (value == null)
-        {
-            Debug.LogErrorFormat("CopyLightingSettings: Could get data from {0}.{1}", type.Name, methodName);
-            return false;
-        }
+            var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic);
+            if (method == null)
+            {
+                Debug.LogErrorFormat("CopyLightingSettings: Could not find {0}.{1}", type.Name, methodName);
+                return false;
+            }
 
-        settings = value;
-        return true;
+            var value = method.Invoke(null, null) as UnityEngine.Object;
+            if (value == null)
+            {
+                Debug.LogErrorFormat("CopyLightingSettings: Could get data from {0}.{1}", type.Name, methodName);
+                return false;
+            }
+
+            settings = value;
+            return true;
+        }
     }
 }
 #endif
