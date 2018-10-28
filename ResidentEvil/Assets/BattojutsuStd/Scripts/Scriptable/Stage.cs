8bb10c43b583534f21980babfb726969278a7a9d
aksharamurda <43597885+aksharamurda@users.noreply.github.com>
2018-10-28 14:52:31 +0700

    Add some script

---
+++ ResidentEvil/Assets/BattojutsuStd/Scripts/Scriptable/Stage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattojutsuStd.Scriptable
+{
+    [CreateAssetMenu(fileName = "Stage", menuName = "Scriptable/Stage", order = 1)]
+    public class Stage : ScriptableObject
+    {
+
+    }
+}
